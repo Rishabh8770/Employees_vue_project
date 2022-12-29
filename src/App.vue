a41b2aab1cc41b9bee92bd2bf663ecb3704be85a
Rishabh  Malewar <rishabh.malewar.20.1@mountblue.tech>
2022-12-29 11:18:34 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Employees/>
+  </div>
+</template>
+<script>
+  import Employees from './components/Employees.vue'
+  export default {
+    name: 'App',
+    components: {
+      Employees
+    }
+  }
+</script>
+
+<style>
+ #app {
+  padding: 20px;
+  width: 100%;
+  text-align: center;
+ }
+</style>
---
+++ src/components/Employees.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1 id="header">Employee Table</h1>
+    <table border="1px" width="100%">
+      <tr>
+        <td>Name</td>
+        <td>salary</td>
+        <td>Age</td>
+      </tr>
+      <tr v-for="item in list" v-bind:key="item.id">
+        <td>{{item.employee_name}}</td>
+        <td>{{item.employee_salary}}</td>
+        <td>{{item.employee_age}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+
+import axios from 'axios'
+
+export default {
+  name: 'Employees',
+  data()
+  {
+    return {list:undefined}
+  },
+  mounted () {
+    console.log("Hello")
+    axios
+      .get('https://dummy.restapiexample.com/api/v1/employees')
+      .then(res => {
+        this.list = res.data.data;
+        console.warn(res.data.data)
+      })
+  }
+}
+</script>
+<style>
+  #header {
+    color: blue;
+    border: 2px solid black;
+    margin: 1rem;
+  }
+</style>
